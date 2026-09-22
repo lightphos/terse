c33eb8a907cfcbccf3838e8fc29a8c64bfb9a4cb
Satish R <satishramjee@hotmail.com>
2026-09-22 13:48:57 +0100

    Fix http api

--- support/pgserver.py
+++
@@ -1,8 +0,0 @@
-import pgserver
-
-# Start an embedded Postgres server (data stored in ./pgdata)
-db = pgserver.get_server("./pgdata")
-
-# Get a connection URI to use with any driver
-uri = db.get_uri()
-print(uri)  # e.g. postgresql://postgres@localhost:5432/postgres   
